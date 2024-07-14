41484c50bd62289f5c333c9fdd0985571a0c7c84
javad.bagirov <j.bagirov@expressbank.az>
2024-07-14 14:05:37 +0400

    gradle configured

---
+++ onboarding-service/src/test/java/app/kubix/onboarding/OnboardingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kubix.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
